681eddf24b33b86fe55948b9490c0eb70bdbb7e6
Daniel Gagné <dgagne@influencecommunication.com>
2019-11-14 15:17:57 -0500

    Create and respond with JWT

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Add JWT-based authentication to a Node/Express/Knex/Oracle APP.
 ## Authentication
 * [x] Create Server
 * [x] Add auth router
+* [x] Login user with POST api/auth/login
+  * [x] Check if username is in the db
+  * [x] Create and sign a JWT
+  * [x] Respond with JWT
+  * [ ] Create a login form
+  * [ ] Validate required field
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,18 +1,44 @@
 const db = require('../services/database');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.login = async (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
 
   const user = await db
-    .select('ENCRYPTED_PASSWORD')
+    .select('ID', 'USERNAME', 'ENCRYPTED_PASSWORD')
     .from('USERS')
     .where('USERNAME', username);
 
   if (user[0]) {
     bcrypt.compare(password, user[0].ENCRYPTED_PASSWORD).then(result => {
-      res.json({ result });
+      if (result) {
+        const payload = {
+          id: user[0].ID,
+          username: user[0].USERNAME
+
+        };
+        jwt.sign(payload, process.env.TOKEN_SECRET, {
+          expiresIn: '1d'
+        }, (err, token) => {
+
+          if (err) {
+            res.status(422);
+            const error = new Error('Unable to login.');
+            next(error);
+          } else {
+            res.json({
+              token
+            })
+          }
+
+        })
+      } else {
+        res.status(422);
+        const error = new Error('Unable to login.');
+        next(error);
+      }
     });
   } else {
     res.status(422);
