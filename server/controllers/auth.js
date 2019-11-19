7523b4354c034e959d33d95d25670564e44d0296
Daniel Gagné <dgagne@influencecommunication.com>
2019-11-19 09:39:56 -0500

    Add company name to req.user object

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,17 +7,18 @@ exports.login = async (req, res, next) => {
   const password = req.body.password;
 
   const user = await db
-    .select('ID', 'USERNAME', 'FIRSTNAME', 'LASTNAME', 'ENCRYPTED_PASSWORD')
-    .from('USERS')
-    .where('USERNAME', username);
+    .select('COMPANIES.NAME', 'USERS.ID', 'USERS.FIRST_NAME', 'USERS.LAST_NAME', 'USERS.ENCRYPTED_PASSWORD')
+    .from('USERS').innerJoin('COMPANIES', 'USERS.COMPANY_ID', 'COMPANIES.ID')
+    .where('USERS.USERNAME', username);
 
   if (user[0]) {
     bcrypt.compare(password, user[0].ENCRYPTED_PASSWORD).then(result => {
       if (result) {
         const payload = {
           id: user[0].ID,
-          username: user[0].USERNAME
-
+          firstname: user[0].FIRST_NAME,
+          lastname: user[0].LAST_NAME,
+          company: user[0].NAME,
         };
         jwt.sign(payload, process.env.TOKEN_SECRET, {
           expiresIn: '1d'
